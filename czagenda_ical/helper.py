c4eac41130760868c5c0c97f3333a9106caaf2b1
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-20 17:15:19 +0100

    aliases

--- czagenda_ical/helper.py
+++ czagenda_ical/helper.py
@@ -263,6 +263,7 @@ class CzAgendaHelper(object):
                      OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, 
                      "http://%s:%s/api" % (API_HOST, API_PORT))
         
+        pattern = api.replace_search_aliases(pattern)
         pattern = api.parse_search_pattern(pattern)
         count = api.search_event_count(pattern)
         
